d7ecc1d8e2a463d8b1ffa89a0300b0196315e446
FrekeNobles <noblendifreke@gmail.com>
2025-10-19 07:41:39 +0100

    feat.:  addpassword reset page

--- dashboards/src/App.tsx
+++ dashboards/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import SignUpPage from './Pages/SignUpPage'
 import VerifyEmail from './Pages/VerifyEmail'
 import LogInPage from './Pages/LogInPage'
+import PasswordReset from './Pages/PasswordReset'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<SignUpPage />} />
         <Route path="/verifyemail" element={<VerifyEmail/>} />
         <Route path="/login" element={<LogInPage/>} />
+        <Route path="/passwordreset" element={<PasswordReset/>} />
       </Routes>
     </Router>
   )
